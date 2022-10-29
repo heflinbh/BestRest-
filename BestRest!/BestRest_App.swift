35272da7459af4ffc7206c4c7fa2a23c012e7490
heflinbh <heflinbh@miamioh.edu>
2022-10-29 15:12:29 -0400

    Initial Commit

---
+++ BestRest!/BestRest_App.swift
@@ -0,0 +1,17 @@
+//
+//  BestRest_App.swift
+//  BestRest!
+//
+//  Created by Benjamin Heflin on 10/29/22.
+//
+
+import SwiftUI
+
+@main
+struct BestRest_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
